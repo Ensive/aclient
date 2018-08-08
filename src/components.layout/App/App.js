a576cc0adedd5bc09b9c1705d30969dff76fe2f8
Anton Goncharuk <antonge92@gmail.com>
2018-08-08 08:39:22 +0300

    Push changes

--- src/components.layout/App/App.js
+++ src/components.layout/App/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 
 import React, { Component } from 'react'
+// TODO: shall we consider to replace BrowserRouter with HashRouter for server rendering ?
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Home from '../../components.pages/Home'
@@ -9,7 +10,14 @@ import About from '../../components.pages/About'
 import Contact from '../../components.pages/Contact'
 import Header from '../Header/Header'
 
+import posts from '../../../data/posts.json'
+
 class App extends Component {
+  constructor () {
+    super()
+    this.state = { posts: posts }
+  }
+
   render () {
     return (
       <Router>
@@ -19,7 +27,7 @@ class App extends Component {
           <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/posts' component={Home} />
-          <Route path='/posts/:id' component={Single} />
+          <Route path='/posts/:id' render={(props) => <Single posts={posts} {...props} />} />
           <Route path='/contact/' component={Contact} />
         </div>
       </Router>
--- src/components.pages/Single.js
+++ src/components.pages/Single.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import Post from '../components.common/Post/Post'
 
-function Single () {
-  return <Post />
+const { string, object } = PropTypes
+const propTypes = {}
+
+function Single ({ match, posts }) {
+  // const post = posts.find({ id: match.params.id })
+  console.log(match)
+  console.log(match.params.id)
+  console.log(posts)
+  // return <Post />
+  return null
 }
+
+Single.propTypes = propTypes
 
 export default Single
